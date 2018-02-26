513f9cdc99c3c39f4bbca3a6e0cc1726dbbf8dd2
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-02-26 16:18:00 -0700

    body parser update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors());
 
 app.get("/advanceds", (request, response) => {
